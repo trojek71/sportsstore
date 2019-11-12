687394068f0941687adc87e5db4259e5d1c6825e
tomekr <tomekr@gmail.com>
2019-11-12 10:35:20 +0100

    Fix some problems

---
+++ data.js
@@ -0,0 +1,27 @@
+var data = [{ id: 1, name: "Kajak", category: "Sporty wodne",
+                description: "Kajak dla jednej osoby", price: 275 },
+            { id: 2, name: "Kamizelka ratunkowa", category: "Sporty wodne",
+                description: "Skuteczna i modna", price: 48.95 },
+            { id: 3, name: "Piłka nożna", category: "Piłka nożna",
+                description: "Zaakceptowana przez federację pod względem wagi i wymiarów", price: 19.50 },
+            { id: 4, name: "Chorągiewki do rogów boiska", category: "Piłka nożna",
+                description: "Odmień swoje boisko, nadając mu profesjonalny sznyt", price: 34.95 },
+            { id: 5, name: "Stadion", category: "Piłka nożna",
+                description: "Stadion na 35000 miejsc", price: 79500 },
+            { id: 6, name: "Myśląca czapeczka", category: "Szachy",
+                description: "Zwiększa wydajność mózgu o 75%", price: 16 },
+            { id: 7, name: "Chwiejne krzesło", category: "Szachy",
+                description: "Potajemnie doprowadź przeciwnika do irytacji", price: 29.95 },
+            { id: 8, name: "Szachownica", category: "Szachy",
+                description: "Zabawa dla całej rodziny", price: 75 },
+            { id: 9, name: "Król(u) złoty", category: "Szachy",
+                description: "Pozłacana, zdobiona diamentami figura króla", price: 1200 }]
+
+module.exports = function () {
+    return {
+        products: data,
+        categories: [...new Set(data.map(p => p.category))].sort(),
+        orders: []
+    } 
+}
+
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.min.css";
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app");
